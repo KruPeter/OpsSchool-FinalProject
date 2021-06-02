75fe43b82c94c24e795ee34fffeff554eded3e10
KruPeter <53606842+KruPeter@users.noreply.github.com>
2021-06-02 21:24:07 +0300

    Add files via upload

--- consul.tf
+++ consul.tf
@@ -1,21 +1,36 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 # Create an aws private instance with ubuntu and consul on it
 resource "aws_instance" "consul-server" {
-  count                       = "${var.servers}"
-  ami                         = "${data.aws_ami.ubuntu.id}"
+  count                       = 3
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  subnet_id                   = "${element(aws_subnet.private.*.id, count.index)}"
+  subnet_id                   = element(aws_subnet.private.*.id, count.index)
   key_name                    = aws_key_pair.VPC-project-key.key_name
-  iam_instance_profile        = "${aws_iam_instance_profile.consul-join.name}"
+  iam_instance_profile        = aws_iam_instance_profile.consul-join.name
   vpc_security_group_ids      = ["${aws_security_group.project_consul.id}"]
+  depends_on                  = [aws_nat_gateway.gw]
 
-  user_data = "${element(data.template_cloudinit_config.consul_server.*.rendered, count.index)}"
+  user_data = "element(data.template_cloudinit_config.consul_server.*.rendered, count.index)"
 
   tags = {
     Name = "project-consul-server-${count.index+1}"
     consul_server = "true"
   }
-
-  depends_on = [aws_nat_gateway.gw]
 }
 
 # Create the user-data for the Consul server
